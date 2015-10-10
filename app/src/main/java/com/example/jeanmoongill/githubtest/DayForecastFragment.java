3c3deb3e9d4bb73a1da71f83bdf171dd4ae314c8
jeanmoongill <moongill2008@gmail.com>
2015-10-10 10:50:59 +0900

    add weather code for example

--- app/src/main/java/com/example/jeanmoongill/githubtest/DayForecastFragment.java
+++ app/src/main/java/com/example/jeanmoongill/githubtest/DayForecastFragment.java
@@ -6,6 +6,7 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.*;
 
 /**
  * Created by jeanmoongill on 2015. 10. 10..
@@ -14,6 +15,16 @@ public class DayForecastFragment extends Fragment{
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
-    }
+
+        View v = inflater.inflate(R.layout.dayforecast_fragment, container, false);
+
+        TextView tempView = (TextView) v.findViewById(R.id.tempForecast);
+        TextView descView = (TextView) v.findViewById(R.id.skydescForecast);
+        tempView.setText((int)(dayForecast.forecastTemp.min - 275.15) + "-" + (int) (dayForecast.forecastTemp.max - 275.15));
+        descView.setText(dayForecast.weather.currentCondition.getDescr());
+        iconWeather = (ImageView) v.findViewById(R.id.forCondIcon);
+        JSONIconWeatherTask task = new JSONIconWeatherTask();
+        task.execute(new String[]{dayForecast.weather.currentCondition.getIcon()});
+
+        return v;
 }
